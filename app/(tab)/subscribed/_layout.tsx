dd0f9a93903d638a81031f29cd5c0d2f522871cb
lateNightDebug <luna.mccormick@edu.sait.ca>
2026-03-01 23:35:52 -0700

    Hidden headers for comics

--- app/(tab)/subscribed/_layout.tsx
+++ app/(tab)/subscribed/_layout.tsx
@@ -2,15 +2,9 @@ import { Stack } from "expo-router";
 
 export default function SettingsLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ title: "My", headerShown: false }}
-      />
-       <Stack.Screen
-        name="comics"
-        options={{ title: "COMICS", headerShown: false }}
-      />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ title: "My" }} />
+      <Stack.Screen name="comics" options={{ title: "COMICS" }} />
     </Stack>
   );
 }
--- app/(tab)/subscribed/index.tsx
+++ app/(tab)/subscribed/index.tsx
@@ -1,26 +1,28 @@
-import { StyleSheet, Text, View, StatusBar, Pressable, Image, FlatList, ScrollView } from 'react-native'
-import { useRouter } from 'expo-router';
+import { useRouter } from "expo-router";
+import {
+  Pressable,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-import ComicCard from '@/components/comic-card';
-import { comics } from '@/data/comics';
+import ComicCard from "@/components/comic-card";
 import { theme } from "../../../styles/theme";
-
 
 const router = useRouter();
 
 export default function Index() {
-  
-
   return (
     <View
-          style={{
-            flex: 4,
-            paddingTop: StatusBar.currentHeight,
-            backgroundColor: theme.colors.bg,
-            minWidth: 350,
-            maxWidth: "100%",
-          }}
-          
+      style={{
+        flex: 4,
+        paddingTop: StatusBar.currentHeight,
+        backgroundColor: theme.colors.bg,
+        minWidth: 350,
+        maxWidth: "100%",
+      }}
     >
       <Text style={styles.header}>My Series</Text>
       <View
@@ -31,9 +33,7 @@ export default function Index() {
           borderColor: theme.colors.border,
           padding: theme.spacing.card,
         }}
-      >
-          
-      </View>  
+      ></View>
       <View
         style={{
           flex: 8,
@@ -44,12 +44,12 @@ export default function Index() {
           justifyContent: "space-between",
           padding: theme.spacing.card,
         }}
-        >
-          <Text style={styles.text}>RECENT</Text>
-          <Text style={styles.text}>SUBSCRIBED</Text>
-          <Text style={styles.text}>DOWNLOADS</Text>
-          <Text style={styles.text}>UNLOCKED</Text>
-          <Text style={styles.text}>CREATORS</Text>
+      >
+        <Text style={styles.text}>RECENT</Text>
+        <Text style={styles.text}>SUBSCRIBED</Text>
+        <Text style={styles.text}>DOWNLOADS</Text>
+        <Text style={styles.text}>UNLOCKED</Text>
+        <Text style={styles.text}>CREATORS</Text>
       </View>
       <View
         style={{
@@ -58,136 +58,130 @@ export default function Index() {
           borderBottomWidth: 1,
           borderColor: theme.colors.border,
           flexDirection: "row",
-          justifyContent: "left",
+          justifyContent: "flex-start",
           padding: theme.spacing.card,
         }}
       >
-          <Text style={styles.text}>10 SERIES TOTAL</Text>
-          <Text style={styles.text}>4 UNREAD SERIES</Text>
-      </View> 
-      <View>  
-      <ScrollView
-      
+        <Text style={styles.text}>10 SERIES TOTAL</Text>
+        <Text style={styles.text}>4 UNREAD SERIES</Text>
+      </View>
+      <View>
+        <ScrollView
           style={{
             marginTop: 40,
-            
-          }}>
-            <View>
-              <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
-                <ComicCard 
-                  id="1"
-                  thumb= {require("@/assets/images/ComicPreview1.png")}
-                  title= 'war and peas'
-                  episode={2}
-                  notif='last read yesterday'
-                  />
-                </Pressable>
-              </View>
-              <View>
-                <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
-                  <ComicCard 
-                    id="2"
-                    thumb= {require("@/assets/images/ComicPreview2.png")}
-                    title= 'some comic'
-                    episode={2}
-                    notif='last read yesterday'
-                    />
-                </Pressable>
-              </View>
-              <View>
-                <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
-                  <ComicCard 
-                    id={"3"}
-                    thumb= {require("@/assets/images/ComicPreview3.png")}
-                    title= {'another comic'}
-                    episode={2}
-                    notif={'last read yesterday'}
-                    />
-                </Pressable>
-              </View>
-              <View>
-                <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
-                  <ComicCard 
-                    id={"4"}
-                    thumb= {require("@/assets/images/ComicPreview4.png")}
-                    title= {'probably gay'}
-                    episode={2}
-                    notif={'last read yesterday'}
-                    />
-                </Pressable>
-              </View>
-              <View>
-                <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
-                  <ComicCard 
-                    id={"5"}
-                    thumb= {require("@/assets/images/ComicPreview5.png")}
-                    title= {'mostly gay'}
-                    episode={2}
-                    notif={'last read yesterday'}
-                    />
-                </Pressable>
-              </View>
-              <View>
-                <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
-                  <ComicCard 
-                    id={"6"}
-                    thumb= {require("@/assets/images/ComicPreview6.png")}
-                    title= {'het slop'}
-                    episode={2}
-                    notif={'last read yesterday'}
-                    />
-                </Pressable>
-              </View>
-              <View>
-                <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
-                  <ComicCard 
-                    id={"7"}
-                    thumb= {require("@/assets/images/ComicPreview1.png")}
-                    title= {'nothing special'}
-                    episode={2}
-                    notif={'last read yesterday'}
-                    />
-                </Pressable>
-                </View>
-                <View>
-                <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
-                  <ComicCard 
-                    id={"8"}
-                    thumb= {require("@/assets/images/ComicPreview2.png")}
-                    title= {'something special'}
-                    episode={2}
-                    notif={'last read yesterday'}
-                    />
-                </Pressable>
-              
-              </View>
+          }}
+        >
+          <View>
+            <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
+              <ComicCard
+                id="1"
+                thumb={require("@/assets/images/ComicPreview1.png")}
+                title="war and peas"
+                episode={2}
+                notif="last read yesterday"
+              />
+            </Pressable>
+          </View>
+          <View>
+            <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
+              <ComicCard
+                id="2"
+                thumb={require("@/assets/images/ComicPreview2.png")}
+                title="some comic"
+                episode={2}
+                notif="last read yesterday"
+              />
+            </Pressable>
+          </View>
+          <View>
+            <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
+              <ComicCard
+                id={"3"}
+                thumb={require("@/assets/images/ComicPreview3.png")}
+                title={"another comic"}
+                episode={2}
+                notif={"last read yesterday"}
+              />
+            </Pressable>
+          </View>
+          <View>
+            <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
+              <ComicCard
+                id={"4"}
+                thumb={require("@/assets/images/ComicPreview4.png")}
+                title={"probably gay"}
+                episode={2}
+                notif={"last read yesterday"}
+              />
+            </Pressable>
+          </View>
+          <View>
+            <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
+              <ComicCard
+                id={"5"}
+                thumb={require("@/assets/images/ComicPreview5.png")}
+                title={"mostly gay"}
+                episode={2}
+                notif={"last read yesterday"}
+              />
+            </Pressable>
+          </View>
+          <View>
+            <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
+              <ComicCard
+                id={"6"}
+                thumb={require("@/assets/images/ComicPreview6.png")}
+                title={"They Were Roommates"}
+                episode={2}
+                notif={"last read yesterday"}
+              />
+            </Pressable>
+          </View>
+          <View>
+            <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
+              <ComicCard
+                id={"7"}
+                thumb={require("@/assets/images/ComicPreview1.png")}
+                title={"nothing special"}
+                episode={2}
+                notif={"last read yesterday"}
+              />
+            </Pressable>
+          </View>
+          <View>
+            <Pressable onPress={() => router.push("../(tab)/subscribed/comic")}>
+              <ComicCard
+                id={"8"}
+                thumb={require("@/assets/images/ComicPreview2.png")}
+                title={"something special"}
+                episode={2}
+                notif={"last read yesterday"}
+              />
+            </Pressable>
+          </View>
         </ScrollView>
-        </View>
+      </View>
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
-    container: {
-      marginTop: 40,
-    },
-    comicThumb: {
-
-    },
-    text: {
-      color: "white",
+  container: {
+    marginTop: 40,
+  },
+  comicThumb: {},
+  text: {
+    color: "white",
     fontWeight: "500",
     lineHeight: 20,
     fontSize: 12,
     margin: 10,
-    },
-    header: {
+  },
+  header: {
     color: "white",
     fontWeight: "600",
     lineHeight: 30,
     fontSize: 20,
     margin: 10,
   },
-})
+});
